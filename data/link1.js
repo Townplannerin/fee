162edafd16e0418117c498b3a4561c9b0de2b9b0
Townplannerin <34913596+Townplannerin@users.noreply.github.com>
2023-01-18 19:26:42 +0530

    Add files via upload

---
+++ data/link1.js
@@ -0,0 +1,2 @@
+var sslr = L.esri.tiledMapLayer({
+            url: 'https://kgis.ksrsac.in/kgismaps1/rest/services/CadastralData_Admin/Cached_CadastralData_Admin/MapServer', maxZoom: 21})
---
+++ data/link2.js
@@ -0,0 +1,2 @@
+var sslr = L.esri.tiledMapLayer({
+            url: 'https://kgis.ksrsac.in/kgismaps2/rest/services/CadastralData_Admin/Cached_CadastralData_Admin/MapServer', maxZoom: 21})
